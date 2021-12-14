9320624bfb4a73c5648e4f63b926d10114f78a30
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2021-12-14 17:26:50 +0200

    core: Implement `round_to_grid` for `Point` (#4)
    A simple function that fits a point to a grid of `WIDTH * HEIGHT *
    NUM_CHANNELS`, with `pixel_size * pixel_size` subpixels

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,28 @@
 pub struct Point {
-    x: f64,
-    y: f64,
+    pub x: f64,
+    pub y: f64,
+}
+
+pub struct GridPoint {
+    pub x: (i64, i64),
+    pub y: (i64, i64),
+}
+
+impl Point {
+    pub fn round_to_grid(&self, pixel_size: usize) -> GridPoint {
+        let round_on_axis = |axis: f64| -> (i64, i64) {
+            let pixel_length = 1_f64 / (pixel_size as f64);
+            let f = axis.fract();
+            let cell_index = f / pixel_length;
+
+            return (axis.trunc() as i64, cell_index as i64);
+        };
+
+        return GridPoint {
+            x: round_on_axis(self.x),
+            y: round_on_axis(self.y),
+        };
+    }
 }
 
 pub enum Primitive {
