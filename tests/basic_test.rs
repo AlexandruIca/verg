711858ade633b3613e3e53b96bb3ce5d91752185
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2021-12-14 14:11:11 +0200

    test: Update `basic_test.rs` with update interface of `verg` (#5)

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,26 +1,28 @@
 // This test should just write a gray 100x100 image into `basic_test.png`.
 
-use verg::canvas::{Canvas, NUM_CHANNELS};
+use verg::canvas::{Canvas, CanvasDescription};
 
 #[test]
 fn basic_test() {
-    const WIDTH: i64 = 100;
-    const HEIGHT: i64 = 100;
-    const BUFFER_SIZE: usize = (WIDTH * HEIGHT * NUM_CHANNELS) as usize;
-    let mut buffer: [f64; BUFFER_SIZE] = [0.5f64; BUFFER_SIZE];
+    const WIDTH: usize = 100;
+    const HEIGHT: usize = 100;
 
-    let canvas = Canvas::from_buffer(&mut buffer, WIDTH, HEIGHT).unwrap();
-    assert_eq!(canvas.width, WIDTH);
-    assert_eq!(canvas.height, HEIGHT);
+    let canvas = Canvas::new(CanvasDescription {
+        width: WIDTH,
+        height: HEIGHT,
+        ..Default::default()
+    });
 
-    let mut u8_buffer: [u8; BUFFER_SIZE] = [0u8; BUFFER_SIZE];
-    canvas.to_u8(&mut u8_buffer).unwrap();
+    assert_eq!(canvas.desc.width, WIDTH);
+    assert_eq!(canvas.desc.height, HEIGHT);
+
+    let u8_buffer = canvas.to_u8();
 
     image::save_buffer(
         "basic_test.png",
-        &u8_buffer,
-        WIDTH as u32,
-        HEIGHT as u32,
+        u8_buffer.as_slice(),
+        canvas.desc.width as u32,
+        canvas.desc.height as u32,
         image::ColorType::Rgba8,
     )
     .unwrap();
