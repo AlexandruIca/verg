59bd20ed2189202e761f3ec977fe5a99b574d293
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-04-03 22:25:02 +0300

    core: Change the line rendering algorithm (#4)
    The old algorithm was a bit too complex to implement correctly. I was
    stuck with the coverage computation, I just couldn't find a way to
    compute the trapezoidal area correctly and robustly.
    
    Being tired, I decided to follow `font-rs`'s choice of line rendering
    algorithm with Levien-style anti-aliasing. It gives excellent results
    and (at least at first) seems kind of easy to understand. I'll have to
    see how I can implement the even-odd fill rule with this one. Right now
    I'm thinking about checking when the coverage changes its sign, but I
    don't know if that's the correct way, we'll see.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,11 +1,10 @@
 use crate::color::{Color, FillRule, FillStyle};
-use crate::geometry::{intersect_line_with_grid, GridPoint, Path, PathOps, Point};
+use crate::geometry::{Path, PathOps, Point};
 use std::vec::Vec;
 
 #[derive(Debug, Clone)]
-struct AccumulationCell {
-    cover: u8,
-    area: u8,
+pub struct AccumulationCell {
+    pub area: f64,
 }
 
 #[derive(Debug)]
@@ -53,10 +52,7 @@ impl Canvas {
 
         Canvas {
             buffer,
-            accumulation_buffer: vec![
-                AccumulationCell { cover: 0, area: 0 };
-                desc.width * desc.height
-            ],
+            accumulation_buffer: vec![AccumulationCell { area: 0.0_f64 }; desc.width * desc.height],
             desc,
         }
     }
@@ -74,10 +70,19 @@ impl Canvas {
             .collect::<Vec<u8>>();
     }
 
-    fn buffer_set_at(&mut self, x: i64, y: i64, color: &Color) {
+    fn _buffer_set_at(&mut self, x: i64, y: i64, color: &Color) {
         let xu = x as usize;
         let yu = y as usize;
         let starting: usize = yu * self.desc.width * NUM_CHANNELS + xu * NUM_CHANNELS;
+
+        self.buffer[starting] = color.r;
+        self.buffer[starting + 1] = color.g;
+        self.buffer[starting + 2] = color.b;
+        self.buffer[starting + 3] = color.a;
+    }
+
+    fn buffer_set_at_usize(&mut self, x: usize, y: usize, color: &Color) {
+        let starting: usize = y * self.desc.width * NUM_CHANNELS + x * NUM_CHANNELS;
 
         self.buffer[starting] = color.r;
         self.buffer[starting + 1] = color.g;
@@ -98,33 +103,159 @@ impl Canvas {
         }
     }
 
+    fn buffer_get_at_usize(&self, x: usize, y: usize) -> Color {
+        let starting: usize = y * self.desc.width * NUM_CHANNELS + x * NUM_CHANNELS;
+
+        Color {
+            r: self.buffer[starting],
+            g: self.buffer[starting + 1],
+            b: self.buffer[starting + 2],
+            a: self.buffer[starting + 3],
+        }
+    }
+
+    fn _accumulation_buffer_set_at(&mut self, x: i64, y: i64, _area: i64) {
+        let _xu = x as usize;
+        let _yu = y as usize;
+
+        // self.accumulation_buffer[yu * self.desc.width + xu].area += area as u8;
+    }
+
     ///
     /// # Note
     ///
     /// Expects `start` and `end` to be lexicographically sorted (by `x` and `y`).
     ///
+    /// Bresenham's line drawing algorithm, adapted from here:
+    /// - https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm
+    ///
+    /// Line drawing algorithm taken from here:
+    /// - https://medium.com/@raphlinus/inside-the-fastest-font-renderer-in-the-world-75ae5270c445
+    ///
     fn draw_line(&mut self, start: &Point, end: &Point) {
-        let start = start.round_to_grid(self.desc.pixel_size);
-        let end = end.round_to_grid(self.desc.pixel_size);
-        let points = intersect_line_with_grid(&start, &end, &self.desc);
+        let update = |area: f64, cell: &mut AccumulationCell| {
+            cell.area += area;
+        };
+        let p0 = start;
+        let p1 = end;
 
-        let first = points.first().unwrap();
-        self.buffer_set_at(first.x.0, first.y.0, &Color::default());
+        if (p0.y - p1.y).abs() <= f64::EPSILON {
+            return;
+        }
+        let (dir, p0, p1) = if p0.y < p1.y {
+            (1.0, p0, p1)
+        } else {
+            (-1.0, p1, p0)
+        };
+        let dxdy = (p1.x - p0.x) / (p1.y - p0.y);
+        let mut x = p0.x;
+        let y0 = p0.y as usize;
 
-        points.windows(2).for_each(|p: &[GridPoint]| {
-            let a = &p[0];
-            let b = &p[1];
+        for y in y0..self.desc.height.min(p1.y.ceil() as usize) {
+            let linestart = y * self.desc.width;
+            let dy = ((y + 1) as f64).min(p1.y) - (y as f64).max(p0.y);
+            let xnext = x + dxdy * dy;
+            let d = dy * dir;
+            let (x0, x1) = if x < xnext { (x, xnext) } else { (xnext, x) };
+            let x0floor = x0.floor();
+            let x0i = x0floor as i32;
+            let x1ceil = x1.ceil();
+            let x1i = x1ceil as i32;
+            if x1i <= x0i + 1 {
+                let xmf = 0.5 * (x + xnext) - x0floor;
+                let linestart_x0i = linestart as isize + x0i as isize;
+                if linestart_x0i < 0 {
+                    continue; // oob index
+                }
+                // self.accumulation_buffer[linestart_x0i as usize].area += d - d * xmf;
+                // self.accumulation_buffer[linestart_x0i as usize + 1].area += d * xmf;
+                update(
+                    d - d * xmf,
+                    &mut self.accumulation_buffer[linestart_x0i as usize],
+                );
+                update(
+                    d * xmf,
+                    &mut self.accumulation_buffer[linestart_x0i as usize + 1],
+                );
+            } else {
+                let s = (x1 - x0).recip();
+                let x0f = x0 - x0floor;
+                let a0 = 0.5 * s * (1.0 - x0f) * (1.0 - x0f);
+                let x1f = x1 - x1ceil + 1.0;
+                let am = 0.5 * s * x1f * x1f;
+                let linestart_x0i = linestart as isize + x0i as isize;
+                if linestart_x0i < 0 {
+                    continue; // oob index
+                }
+                // self.accumulation_buffer[linestart_x0i as usize].area += d * a0;
+                update(
+                    d * a0,
+                    &mut self.accumulation_buffer[linestart_x0i as usize],
+                );
 
-            if (a.x.0 - b.x.0).abs() > 1 || (a.y.0 - b.y.0).abs() > 1 {
-                let (x, y) = (((a.x.0 + b.x.0) / 2), ((a.y.0 + b.y.0) / 2));
-                self.buffer_set_at(x, y, &Color::default());
+                if x1i == x0i + 2 {
+                    /*
+                    self.accumulation_buffer[linestart_x0i as usize + 1].area +=
+                        d * (1.0 - a0 - am);
+                        */
+                    update(
+                        d * (1.0 - a0 - am),
+                        &mut self.accumulation_buffer[linestart_x0i as usize + 1],
+                    );
+                } else {
+                    let a1 = s * (1.5 - x0f);
+                    // self.accumulation_buffer[linestart_x0i as usize + 1].area += d * (a1 - a0);
+                    update(
+                        d * (a1 - a0),
+                        &mut self.accumulation_buffer[linestart_x0i as usize + 1],
+                    );
+
+                    for xi in x0i + 2..x1i - 1 {
+                        // self.accumulation_buffer[linestart + xi as usize].area += d * s;
+                        update(
+                            d * s,
+                            &mut self.accumulation_buffer[linestart + xi as usize],
+                        );
+                    }
+                    let a2 = a1 + (x1i - x0i - 3) as f64 * s;
+                    /*
+                    self.accumulation_buffer[linestart + (x1i - 1) as usize].area +=
+                        d * (1.0 - a2 - am);
+                    */
+                    update(
+                        d * (1.0 - a2 - am),
+                        &mut self.accumulation_buffer[linestart + (x1i - 1) as usize],
+                    );
+                }
+                // self.accumulation_buffer[linestart + x1i as usize].area += d * am;
+                update(
+                    d * am,
+                    &mut self.accumulation_buffer[linestart + x1i as usize],
+                );
             }
-
-            self.buffer_set_at(b.x.0, b.y.0, &Color::default());
-        });
+            x = xnext;
+        }
     }
 
-    pub fn draw_shape(&mut self, path: Path, _fill_style: FillStyle, _fill_rule: FillRule) {
+    pub fn draw_shape(&mut self, path: Path, fill_style: FillStyle, _fill_rule: FillRule) {
+        let (mut min_x, mut min_y) = (usize::MAX, usize::MAX);
+        let (mut max_x, mut max_y) = (usize::MIN, usize::MIN);
+
+        let mut update_bounds = |x: f64, y: f64| {
+            let x = x as usize;
+            let y = y as usize;
+
+            min_x = usize::min(min_x, x);
+            min_y = usize::min(min_y, y);
+
+            max_x = usize::max(max_x, x);
+            max_y = usize::max(max_y, y);
+        };
+
+        let mut start_point = Point {
+            x: 0.0_f64,
+            y: 0.0_f64,
+        };
         let mut currently_at = Point {
             x: 0.0_f64,
             y: 0.0_f64,
@@ -135,13 +266,28 @@ impl Canvas {
                 PathOps::MoveTo { x, y } => {
                     currently_at.x = *x;
                     currently_at.y = *y;
+
+                    start_point.x = *x;
+                    start_point.y = *y;
+
+                    update_bounds(*x, *y);
                 }
                 PathOps::MoveToRel { x, y } => {
                     currently_at.x += *x;
                     currently_at.y += *y;
+
+                    start_point.x = currently_at.x;
+                    start_point.y = currently_at.y;
+
+                    update_bounds(currently_at.x, currently_at.y);
                 }
                 PathOps::LineTo { x, y } => {
                     self.draw_line(&currently_at, &Point { x: *x, y: *y });
+
+                    currently_at.x = *x;
+                    currently_at.y = *y;
+
+                    update_bounds(*x, *y);
                 }
                 PathOps::LineToRel { x, y } => {
                     self.draw_line(
@@ -151,7 +297,51 @@ impl Canvas {
                             y: currently_at.y + *y,
                         },
                     );
+
+                    currently_at.x += *x;
+                    currently_at.y += *y;
+
+                    update_bounds(currently_at.x, currently_at.y);
                 }
+                PathOps::Close => {
+                    self.draw_line(&currently_at, &start_point);
+
+                    currently_at.x = start_point.x;
+                    currently_at.y = start_point.y;
+                }
+            }
+        }
+
+        for y in min_y..=max_y {
+            let mut acc = 0.0_f64;
+
+            for x in min_x..max_x {
+                let cell = &mut self.accumulation_buffer[y * self.desc.width + x];
+                let area = cell.area;
+                acc += area;
+                cell.area = 0.0_f64;
+
+                let dest = self.buffer_get_at_usize(x, y);
+                let src = match fill_style {
+                    FillStyle::Plain(Color { r, g, b, a: _ }) => Color {
+                        r,
+                        g,
+                        b,
+                        a: acc.abs(),
+                    },
+                };
+
+                self.buffer_set_at_usize(
+                    x,
+                    y,
+                    &Color {
+                        r: src.r * src.a + dest.r * (1.0_f64 - src.a),
+                        g: src.g * src.a + dest.g * (1.0_f64 - src.a),
+                        b: src.b * src.a + dest.b * (1.0_f64 - src.a),
+                        a: dest.a,
+                    }
+                    .clamp(),
+                );
             }
         }
     }
