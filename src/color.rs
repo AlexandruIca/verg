0d591a0cbcb9313699e2f94ab80328f781cc9246
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2022-04-03 22:19:21 +0300

    core: Implement `clamp` method for `Color` (#4)
    This function clamps each color's RGB channel to the range [0.0, 1.0]

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,31 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
     pub r: f64,
     pub g: f64,
     pub b: f64,
     pub a: f64,
+}
+
+fn clamp_f64(v: f64, min: f64, max: f64) -> f64 {
+    return if v < min {
+        min
+    } else if v > max {
+        max
+    } else {
+        v
+    };
+}
+
+impl Color {
+    pub fn clamp(&self) -> Color {
+        let (min, max) = (0.0_f64, 1.0_f64);
+        Color {
+            r: clamp_f64(self.r, min, max),
+            g: clamp_f64(self.g, min, max),
+            b: clamp_f64(self.b, min, max),
+            a: clamp_f64(self.a, min, max),
+        }
+    }
 }
 
 impl Default for Color {
